9c6c14007e5bc97f5aff9c9256ef68d0c18f7ee2
Michalina <michalina.oniszczuk@gmail.com>
2023-11-26 22:04:07 +0100

    username unique

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './user.schema';
@@ -9,8 +9,15 @@ export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-  
+    // Check if the username already exists
+    const existingUser = await this.userModel.findOne({ username: createUserDto.username });
 
+    if (existingUser) {
+      // Handle the case where the username is not unique
+      throw new ConflictException('Username is already taken');
+    }
+
+    // If the username is unique, proceed with creating the user
     const user = new this.userModel(createUserDto);
     return user.save();
   }
@@ -18,5 +25,4 @@ export class UsersService {
   async findOne(username: string): Promise<User | undefined> {
     return this.userModel.findOne({ username });
   }
-  
 }
