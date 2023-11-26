67e67fa865b69901e83d7179b54913f0c99280d4
Michalina <michalina.oniszczuk@gmail.com>
2023-11-26 22:05:32 +0100

    deleting comments

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,15 +9,14 @@ export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    // Check if the username already exists
     const existingUser = await this.userModel.findOne({ username: createUserDto.username });
 
     if (existingUser) {
-      // Handle the case where the username is not unique
+ 
       throw new ConflictException('Username is already taken');
     }
 
-    // If the username is unique, proceed with creating the user
+
     const user = new this.userModel(createUserDto);
     return user.save();
   }
