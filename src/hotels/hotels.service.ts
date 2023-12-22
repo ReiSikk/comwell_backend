5a796e3dc5eb78fc7aaed47eedff84570b592105
Rei Sikk <reix0010@stud.kea.dk>
2023-12-22 18:47:46 +0200

    testing fixes

--- src/hotels/hotels.service.ts
+++ src/hotels/hotels.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Hotel } from './hotel.schema';
 
 @Injectable()
@@ -13,6 +13,9 @@ export class HotelsService {
   }
 
   async getHotelWithRooms(id: string): Promise<Hotel> {
+    if (!id || !Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('Invalid ID');
+    }
     return await this.hotelModel.findById(id).populate('rooms').exec();
   }
 }
